bd5211066f45a2d2d753db3147fca9b1b59f8acd
RubenUT <21354385@utcancun.edu.mx>
2024-03-05 01:01:10 -0600

    commit

--- src/components/ExamenItem.vue
+++ src/components/ExamenItem.vue
@@ -1,9 +1,14 @@
 <template>
     <h2>{{ examen.title }}</h2>
+    <select v-model="selectedOption">
+        <option v-for="(option,index) in examen.options" :key="index" :value="index + 1">{{ option }}</option>
+    </select>
+    <p>Respuesta seleccionada {{ selectedOption }}</p>
 </template>
 <script lang="ts" setup>
-
+import {ref} from 'vue';
 import type { IExamen } from '@/interfaces/IExamen';
-const props = defineProps<{examen : IExamen}>();
+const { examen } = defineProps(['examen']);
+const selectedOption = ref<number>(examen.answer);
 </script>
 <style scoped></style>
--- src/components/ExamenList.vue
+++ src/components/ExamenList.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <ExamenItem v-for="(examen, index) in preguntas" :key="index" :examen="examen"/>
+        <ExamenItem v-for="examen in quizData" :key="examen.id" :examen="examen" />
     </div>
 </template>
-<script lang="ts" setup>
-import { ref } from 'vue'
-import type { IExamen } from '@/interfaces/IExamen';
-import ExamenItem from './ExamenItem.vue';
-const preguntas = ref<IExamen[]>([]);
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ExamenItem from '@/components/ExamenItem.vue';
+import QuizData from '@/data/ExamenData';
+
+const quizData = ref(QuizData);
 </script>
 <style scoped></style>
